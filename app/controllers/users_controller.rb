95abd52d646f8f1e2acc10d9dc503c49a0c9dd72
Tran Anh Tai <taipesie123@gmail.com>
2025-08-01 17:00:12 +0700

    Chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,21 @@
 class UsersController < ApplicationController
   before_action :load_user, only: :show
 
+  # GET /users/:id
   def show; end
 
+   # GET /signup
   def new
     @user = User.new
   end
 
+  # POST /signup
   def create
     @user = User.new user_params
 
     if @user.save
+      reset_session
+      log_in @user
       # Handle a successful save.
       flash[:success] = t(".success_message") # TODO: i18m
       redirect_to @user
